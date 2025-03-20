fc58f22c76a8fea81cde37b3c07110c02eb1ff66
TexusCode <texus.tj@gmail.com>
2025-03-21 01:58:07 +0500

    updated

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="{{ url('texus-assets/tailwind.css') }}">
 <button name="button" type="button"
     class="px-7 py-3 bg-green-500 text-xl text-white font-medium rounded-xl hover:bg-green-600">{{
     $slot }}</button>
