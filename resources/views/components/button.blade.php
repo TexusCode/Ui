a9b48d60b7057f5d72b5ce71aef0fb087848658a
TexusCode <texus.tj@gmail.com>
2025-03-21 02:15:46 +0500

    updated

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,6 @@
-<link rel="stylesheet" href="{{ url('texus-assets/tailwind.css') }}">
+{{--
+<link rel="stylesheet" href="{{ url('texus-assets/tailwind.css') }}"> --}}
 <button name="button" type="button"
     class="px-7 py-3 bg-green-500 text-xl text-white font-medium rounded-xl hover:bg-green-600">{{
     $slot }}</button>
-<script src="{{ url('texus-assets/main.js') }}"></script>
+{{-- <script src="{{ url('texus-assets/main.js') }}"></script> --}}
